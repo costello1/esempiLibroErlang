6b0d587fb29c151cd4791a5ec0c7a840827cfe65
Costello1 <pietro.angelici@studenti.unicam.it>
2021-08-19 15:54:24 +0000

    implement a metod total

--- funs/shop2.erl
+++ funs/shop2.erl
@@ -1,6 +1,9 @@
 -module(shop2).
--export([total/1]).
+-export([total/1, total1/1]).
 -import(list, [map/2, sum/1]).
 
 total(L) ->
     sum(map(fun({What, N}) -> shop:cost(What) * N end, L)).
+
+total1(L) ->
+    sum([shop:cost(A) * B || {A, B} <- L]).
