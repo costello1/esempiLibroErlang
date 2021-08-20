7a505257d0e0e60324afca07e83fa643f102470d
Costello1 <pietro.angelici@studenti.unicam.it>
2021-08-20 10:59:35 +0000

    implement module geometry

--- funs/geometry.erl
+++ funs/geometry.erl
@@ -1,5 +1,5 @@
 -module(geometry).
--export([area/1]).
+-export([area/1, perimeter/1]).
 
 area({rectangole, Width, Height}) -> 
     Width * Height;
@@ -9,3 +9,15 @@ area({circle, R}) ->
     R * R * 3.14;
 area({triangle, Width, Height}) ->
     (Width * Height) / 2.
+
+% il triangolo a tutti i lati diversi e lo calcolo solo per quello 
+% è un esempio facile
+perimeter({triangle, A, B, C}) ->
+    A + B + C;
+perimeter({rectangole, Width, Height}) ->
+    2*(Width + Height);
+perimeter([square, Side]) ->
+    Side * 4;
+perimeter([circle, R]) ->
+    2*3.14*R.
+    
