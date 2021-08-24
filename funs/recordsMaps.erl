a05c3dfb815c67b8fea7f59b8c00e6fb2e047ea7
Costello1 <pietro.angelici@studenti.unicam.it>
2021-08-24 13:13:14 +0000

    ok

--- funs/recordsMaps.erl
+++ funs/recordsMaps.erl
@@ -1,7 +1,7 @@
 -module(recordsMaps).
 -export([map_search_pred/2]).
 
-map_search_pred(Map, Pred) when is_map(Map) ->
+map_search_pred(Map, Pred) ->
      map_search_pred(maps:to_list(Map), Pred);
 map_search_pred([], _) ->
     error("is empty");
